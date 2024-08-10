b6acb9e14aead63e52802b33de655ab24e70ac85
Shuban Biswas (orbi8r) <157096929+orbi8r@users.noreply.github.com>
2024-08-10 15:16:18 +0530

    changed a few training variables

--- Scripts/AIController3D.gd
+++ Scripts/AIController3D.gd
@@ -8,12 +8,13 @@ var controls = [0,0,0,0]
 
 func get_obs() -> Dictionary:
 	var obs := [
-		finish_point.position.x - rc_car.position.x,
-		finish_point.position.z - rc_car.position.z,
+		finish_point.position.x,
+		finish_point.position.z,
+		rc_car.position.x,
+		rc_car.position.z,
 		rc_car.rotation.y,
 		rc_car.linear_velocity.x,
 		rc_car.linear_velocity.z,
-		rc_car.position.x**2 + rc_car.position.z**2
 	]
 	return {"obs": obs}
 
@@ -24,12 +25,10 @@ func get_reward() -> float:
 
 func get_action_space() -> Dictionary:
 	return {
-		"tank_steering": {"size": 4, "action_type": "continuous"},
+		"carcontrol": {"size": 2, "action_type": "continuous"},
 	}
 
 
 func set_action(action) -> void:
-	controls[0] = action["tank_steering"][0]
-	controls[1] = action["tank_steering"][1]
-	controls[2] = action["tank_steering"][2]
-	controls[3] = action["tank_steering"][3]
+	controls[0] = action["carcontrol"][0]
+	controls[1] = action["carcontrol"][1]
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -26,7 +26,10 @@ func _ready():
 func _process(delta):
 	if timereward > 1:
 		timereward -= delta
-
+		
+	#negative reward for distance to finish
+	ai_controller_3d.reward -= 0.00001 * delta * ((finish_point.position.x-rc_car.position.x)**2 + (finish_point.position.z-rc_car.position.z)**2)
+	
 
 func _on_finish_point_body_entered(_body):
 	ai_controller_3d.reward += timereward #positive reward for finish line
@@ -34,5 +37,5 @@ func _on_finish_point_body_entered(_body):
 
 
 func _on_area_3d_body_entered(body):
-	ai_controller_3d.reward -= 10.0
+	ai_controller_3d.reward -= 100.0
 	rc_car.position = Vector3(0,0,0)
